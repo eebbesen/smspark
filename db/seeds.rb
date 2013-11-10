a6e4e67f238beaa29469e4068bbb0b21d055cf98
Torleiv Flatebo <tor@flatebo.org>
2013-11-10 14:52:38 -0600

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+event1 = Event.create([{ date:'2013-11-10T00:00:00.000Z',park:'corcoran',game:'quidditch',start_time:'2013-11-10T18:00:00.000Z',end_time:'2013-11-10T20:00:00.000Z',extra_info:'Audlt and kids welcome!',created_at:'2013-11-09T20:18:37.428Z',updated_at:'2013-11-09T20:18:37.428Z' }])

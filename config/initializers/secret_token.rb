fffd887a6389a6c48c801b7f4c173b4175db74f0
Eric Ebbesen <Eric.Ebbesen@govdelivery.com>
2013-11-10 14:38:04 -0600

    Adding rails stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smspark::Application.config.secret_key_base = '3f4f124a67db479ed8fc06aabc43b6e727f5ac0ca5a2fc319ed69c43a0fb007f0453776f39b93d456ae925440fc4ab16eff1ae79f996934434293199f6b142f8'

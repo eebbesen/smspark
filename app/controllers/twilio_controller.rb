0c0b4a709fa1c086b600e026460bd31630d8ce7c
Torleiv Flatebo <tor@flatebo.org>
2013-11-10 15:47:59 -0600

    Add in the twilio controller

---
+++ app/controllers/twilio_controller.rb
@@ -0,0 +1,34 @@
+class TwilioController < ApplicationController
+
+  # POST /twilio
+  # POST /twilio.json
+  def create
+    #@event = Event.new(event_params)
+    #
+    if(params['Body'])
+      input_string = params['Body']
+
+      Rails.logger.debug 'This is the twilio controller'
+      Rails.logger.debug 'Input: ' + input_string
+
+      # PARK GAME TODAY START_TIME END_TIME INFO
+      
+      @input_arr = input_string.split(' ')
+
+      event1 = Event.new
+      event1.park = @input_arr[0]
+      event1.game = @input_arr[1]
+      event1.date = Date.today
+      #event1.date = @input_arr[2]
+      #event1.start_time = @input_arr[3]
+      #event1.end_time = @input_arr[4]
+      event1.extra_info = @input_arr[5]
+
+      event1.save
+
+    end
+
+    render :nothing => true
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Smspark::Application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  #
+
+  post '/twilio' => 'twilio#create'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
